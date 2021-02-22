d53c817b05dc53b527df0780b7739cddc252200f
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2021-02-22 15:28:46 -0300

    Adcionada a Q1 do exercicio 2 que representa uma data e iniciada a questao 2

---
+++ POO JAVA/Exercicio2/Q1/Data.java
@@ -0,0 +1,11 @@
+package Exercicio2.Q1;
+
+class Data {
+    int dia = 01 , mes = 01 , ano = 2021;
+    
+    void setData(int dia, int mes, int ano) {
+    } 
+    void showData(){
+        System.out.println ("A data é " + 0 + this.dia +"/"+  0 + this.mes+"/"+  this.ano);
+    }
+}

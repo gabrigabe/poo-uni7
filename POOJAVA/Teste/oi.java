5bfbada0d972416e3126e7e682a6381babbed0fe
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2021-04-12 20:34:15 -0300

    master

---
+++ POOJAVA/Teste/oi.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class oi{
+ 
+    public static void main(String args[]){
+    Scanner scan = new Scanner (System.in);
+    System.out.println("Digite: "); 
+    String nome1 = scan.nextLine();
+    System.out.println("Seu nome é :"+ nome1); 
+    System.exit(0);
+} 
+   
+}
